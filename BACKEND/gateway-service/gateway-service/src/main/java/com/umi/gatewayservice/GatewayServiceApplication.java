51931247387e8b0cdc918f8fac02284b95f6ccab
Thahir BAH <thahirbah@gmail.com>
2024-02-14 20:33:32 +0100

    first commit

---
+++ BACKEND/gateway-service/gateway-service/src/main/java/com/umi/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,25 @@
+package com.umi.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
+														DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
+
+}
